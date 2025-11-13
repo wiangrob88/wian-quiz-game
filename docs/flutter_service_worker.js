5b3cff697811cbaf494edc00bebcce0047425fe9
Wian Grobbelaar <wian.grobbelaar@africadatacentres.com>
2025-11-13 09:05:57 +0200

    Update web build for GitHub Pages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -27,14 +27,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "2d97922360ee71504034958263e89055
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "a8ddde485447c2052b6c68f2059260f1",
+"flutter_bootstrap.js": "d743109a3398325da59205b6e779ce87",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "1141cf9257cd7e0d052972d87a2d785b",
-"/": "1141cf9257cd7e0d052972d87a2d785b",
-"main.dart.js": "97c31ea22f587b800981a76104f61da6",
+"index.html": "c9d1070ccf6b5398804bb4d42b6f3feb",
+"/": "c9d1070ccf6b5398804bb4d42b6f3feb",
+"main.dart.js": "8275ac0f2acf6e00c6e27b5cd01eef6b",
 "manifest.json": "032bf3993e174e03d2d58da2748c67fa",
 "version.json": "84fa3c500a583bd0e099877d4e706a65"};
 // The application shell files that are downloaded before a service worker can
