92f5dae29727de438b727771ec7ce1810374e314
Wian Grobbelaar <wian.grobbelaar@africadatacentres.com>
2025-11-13 09:11:00 +0200

    Update web build for GitHub Pages

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "2d97922360ee71504034958263e89055
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "d743109a3398325da59205b6e779ce87",
+"flutter_bootstrap.js": "cae1482976a150035e5d8943f07bfaf0",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
